7dabc49506771042b7e95126a153355798c1d021
lsj <aa494119250@qq.com>
2018-11-05 16:13:27 +0800

    第1次创建

---
+++ src/main/java/com/git/gitdemo01/Gitdemo01Application.java
@@ -0,0 +1,12 @@
+package com.git.gitdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitdemo01Application.class, args);
+    }
+}
